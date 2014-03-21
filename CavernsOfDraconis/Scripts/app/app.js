1e260d814ea8e89193049563b0cefb9a755ee8e6
unknown <kevin.bernfeld@COHS_PC67.olmhealth.com>
2014-03-21 17:13:53 -0400

    Initial commit. Some basic styling/layout, and beginning to play with angular

---
+++ CavernsOfDraconis/Scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿var caverns = angular.module('caverns', []);
+
+caverns.controller('home', ['$scope', function ($scope) {
+    $scope.greeting = "hello!";
+    $scope.gamename = '';
+    $scope.password = '';
+}]);
+
+caverns.controller('game', ['$scope', function($scope) {
+    $scope.name = 'Game Title';
+}]);
